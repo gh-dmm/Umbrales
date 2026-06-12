0ac4ec411292b0bc23e3c7dce328756733026f8b
Diego Martinez <diegomartinezmelendez07@gmail.com>
2026-06-11 18:47:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -338,9 +338,8 @@ class TarjetaAcervo {
     // =========================================================================
     // CORRECCIÓN DEFINITIVA DE ENDPOINT: Cambiado de v1beta a v1 estable
     // =========================================================================
-   const urlGemini = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${AQ.Ab8RN6JwvHT9jL1igTiCvLvg_nRg3W-l-v1MkCmYIZlt2WFwAw}`;
-
-
+  const API_KEY = "AQ.Ab8RN6JwvHT9jL1igTiCvLvg_nRg3W-l-v1MkCmYIZlt2WFwAw";
+        const urlGemini = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
     try {
         const response = await fetch(urlGemini, {
     method: 'POST',
